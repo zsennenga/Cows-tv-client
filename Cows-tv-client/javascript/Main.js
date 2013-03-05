9789c72729fd72d0a53976490790df95f2cbb55a
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-03-05 10:21:03 -0800

    First Revision to bring to final verison

--- Cows-tv-client/javascript/Main.js
+++ Cows-tv-client/javascript/Main.js
@@ -1,9 +1,28 @@
 var widgetAPI = new Common.API.Widget();
 var tvKey = new Common.API.TVKeyValue();
-
-var Main =
+var feedData;
+var index1 = 0;
+var index2 = 1;
+function doAjax() {
+	$.ajaxSetup({
+		async: false
+		});
+	$.getJSON('http://169.237.123.4/cows/includes/ajax.php?callback=?', function(data) {
+			feedData = data;
+		});
+}
+function eventUpdate(){
+	  $('#event1').fadeOut(500);
+	  $('#event2').fadeOut(500, function(){
+		  $('#event1').delay(1000).html(feedData[index1]).fadeIn('fast');
+		  $('#event2').delay(1000).html(feedData[index2]).fadeIn('fast');
+	  });
+	  index1 = (index1 + 2) % 6;
+	  index2 = (index2 + 2) % 6;
+}
+var Main = function()
 {
-
+		
 };
 
 Main.onLoad = function()
@@ -11,6 +30,9 @@ Main.onLoad = function()
 	// Enable key event processing
 	this.enableKeys();
 	widgetAPI.sendReadyEvent();
+	doAjax();
+	setInterval(eventUpdate, 1000*15);
+	setInterval(doAjax, 1000*15*60);
 };
 
 Main.onUnload = function()
@@ -20,7 +42,6 @@ Main.onUnload = function()
 
 Main.enableKeys = function()
 {
-	document.getElementById("anchor").focus();
 };
 
 Main.keyDown = function()
