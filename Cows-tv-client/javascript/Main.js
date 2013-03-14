b2fd5716ea79c7d28bcf8fa6bf49c20cf69852d1
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-03-14 09:05:13 -0700

    Added burn protection, changed format to 3 display rotating

--- Cows-tv-client/javascript/Main.js
+++ Cows-tv-client/javascript/Main.js
@@ -1,24 +1,32 @@
 var widgetAPI = new Common.API.Widget();
 var tvKey = new Common.API.TVKeyValue();
 var feedData;
-var index;
+var bgData;
+var updateInterval;
+var burnInterval;
+var burnIndex = 0;
+var tempHtml;
+var eventIndex = new Array();
+
 function doAjax() {
 	$.ajaxSetup({
 		async: false
 		});
-	$.getJSON('http://169.237.123.4/cows/includes/ajax.php?callback=?', function(data) {
+	$.getJSON('http://169.237.123.4/cows/includes/ajaxEvents.php?callback=?', function(data) {
 			feedData = data;
 			eventUpdate();
-			index = 0;
 		});
+	$.getJSON('http://169.237.123.4/cows/includes/ajaxImages.php?callback=?', function(data) {
+		bgData = data;
+	});
 }
-function eventUpdate(){
+
+function eventUpdate() {
 	  if (feedData[0] == "noEvent")	{
 		 $('#content').html(feedData[1]); 
 	  }
-	  else if (feedData.length > 1)	{
-		  /*$('#content').html(feedData[index]);
-		  index = (index + 1) % feedData.length;*/
+	  else if (feedData.length >= 1 && feedData.length < 3)	{
+		 
 		  $('#content').html(feedData[0]);
 		  for (var i = 1; i <feedData.length;i++){
 			  $('#content').append(feedData[i]);
@@ -27,10 +35,46 @@ function eventUpdate(){
 		  $('.other').css("font-size",700/feedData.length + "%");
 		  $('.event').css("height",100/feedData.length + "%");
 	  }
-	  else	{
-		  $('#content').html(feedData[0]);
-		  //index = 0;
+	  else if (feedData.length >= 3)	{
+		  $('#content').html('');
+		  for (var i = 1; i < 4; i++)	{
+		  	if (eventIndex[i] != -1)	{
+				  $('#content').append(feedData[eventIndex[i]]);
+				  eventIndex[i] = eventIndex[i] + 3;
+				  if (eventIndex[i] > feedData.length) eventIndex[i] = -1;
+			  }
+		  }
+		  if (eventIndex[1] == -1 && eventIndex[2] == -1 && eventIndex[3] == -1) {
+			  eventIndex[1] = 0;
+			  eventIndex[2] = 1;
+			  eventIndex[3] = 2;
+		  }
+		  $('.title').css("font-size",850/3 + "%");
+		  $('.other').css("font-size",700/3 + "%");
+		  $('.event').css("height",100/3 + "%");
 	  }
+}
+function burnProtect()	{
+	clearInterval(updateInterval);
+	tempHtml = $('body').html();
+	$('body').html('');
+	$('body').css('background-color','black');
+	burnInterval = setInterval(doBurn,1000*60);
+	doBurn();
+	setTimeout(clearBurn,1000*3*60);
+}
+function doBurn()	{
+	$('body').html("");
+	$('body').css("background-color","black");
+	$('body').css("background-image","url(http://169.237.123.4/cows/images/"+bgData[burnIndex] + ")");
+	burnIndex = (burnIndex + 1) % bgData.length;
+}
+function clearBurn()	{
+	clearInterval(burnInterval);
+	$('body').css('background-color','white');
+	$('body').html(tempHtml);
+	eventUpdate();
+	updateInterval = setInterval(eventUpdate,1000*5*60);
 }
 var Main = function()
 {
@@ -39,11 +83,14 @@ var Main = function()
 
 Main.onLoad = function()
 {
-	// Enable key event processing
 	this.enableKeys();
 	widgetAPI.sendReadyEvent();
+	eventIndex[1] = 0;
+	eventIndex[2] = 1;
+	eventIndex[3] = 2;
 	doAjax();
-	setInterval(eventUpdate, 1000*10);
+	updateInterval = setInterval(eventUpdate, 1000*5*60);
+	setInterval(burnProtect, 1000*30*60);
 	setInterval(doAjax, 1000*60*60);
 };
 
