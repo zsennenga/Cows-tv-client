5315456c2570a562ea57550f3cd2af7b013bc33f
Zachary Ennenga <zsennenga@ucdavis.edu>
2013-03-20 11:53:32 -0700

    Added check for existence of feedData in eventUpdate Fixed range error with eventUpdate

--- Cows-tv-client/javascript/Main.js
+++ Cows-tv-client/javascript/Main.js
@@ -22,37 +22,39 @@ function doAjax() {
 }
 
 function eventUpdate() {
-	  if (feedData[0] == "noEvent")	{
-		 $('#content').html(feedData[1]); 
-	  }
-	  else if (feedData.length >= 1 && feedData.length < 3)	{
-		 
-		  $('#content').html(feedData[0]);
-		  for (var i = 1; i <feedData.length;i++){
-			  $('#content').append(feedData[i]);
+		if (feedData != null)	{
+		  if (feedData[0] == "noEvent")	{
+			 $('#content').html(feedData[1]); 
 		  }
-		  $('.title').css("font-size",850/feedData.length + "%");
-		  $('.other').css("font-size",700/feedData.length + "%");
-		  $('.event').css("height",100/feedData.length + "%");
-	  }
-	  else if (feedData.length >= 3)	{
-		  $('#content').html('');
-		  for (var i = 1; i < 4; i++)	{
-		  	if (eventIndex[i] != -1)	{
-				  $('#content').append(feedData[eventIndex[i]]);
-				  eventIndex[i] = eventIndex[i] + 3;
-				  if (eventIndex[i] > feedData.length) eventIndex[i] = -1;
+		  else if (feedData.length >= 1 && feedData.length < 3)	{
+			 
+			  $('#content').html(feedData[0]);
+			  for (var i = 1; i <feedData.length;i++){
+				  $('#content').append(feedData[i]);
 			  }
+			  $('.title').css("font-size",850/feedData.length + "%");
+			  $('.other').css("font-size",700/feedData.length + "%");
+			  $('.event').css("height",100/feedData.length + "%");
 		  }
-		  if (eventIndex[1] == -1 && eventIndex[2] == -1 && eventIndex[3] == -1) {
-			  eventIndex[1] = 0;
-			  eventIndex[2] = 1;
-			  eventIndex[3] = 2;
+		  else if (feedData.length >= 3)	{
+			  $('#content').html('');
+			  for (var i = 1; i < 4; i++)	{
+			  	if (eventIndex[i] != -1)	{
+					  $('#content').append(feedData[eventIndex[i]]);
+					  eventIndex[i] = eventIndex[i] + 3;
+					  if (eventIndex[i] >= feedData.length) eventIndex[i] = -1;
+				  }
+			  }
+			  if (eventIndex[1] == -1 && eventIndex[2] == -1 && eventIndex[3] == -1) {
+				  eventIndex[1] = 0;
+				  eventIndex[2] = 1;
+				  eventIndex[3] = 2;
+			  }
+			  $('.title').css("font-size",850/3 + "%");
+			  $('.other').css("font-size",700/3 + "%");
+			  $('.event').css("height",100/3 + "%");
 		  }
-		  $('.title').css("font-size",850/3 + "%");
-		  $('.other').css("font-size",700/3 + "%");
-		  $('.event').css("height",100/3 + "%");
-	  }
+		}
 }
 function burnProtect()	{
 	clearInterval(updateInterval);
